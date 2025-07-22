d08e0262bf2d4cde7c480adac9cfb4fc32d41216
Pramatya Jati <35680922+pramjati02@users.noreply.github.com>
2025-07-22 18:32:59 +0100

    Update summarizer_models.py

--- backend/models/summarizer_models.py
+++ backend/models/summarizer_models.py
@@ -3,6 +3,6 @@ import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-tokenizer = AutoTokenizer.from_pretrained("bart_base_trained")
-model = AutoModelForSeq2SeqLM.from_pretrained("bart_base_trained").to(device)
+tokenizer = AutoTokenizer.from_pretrained("pramjati02/Bart_Base_Trained")
+model = AutoModelForSeq2SeqLM.from_pretrained("pramjati02/Bart_Base_Trained").to(device)
 
