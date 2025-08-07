c98cb4549a70cd8ac3627b74b61b7feceddd9e90
pramjati02 <pramatyajati87@gmail.com>
2025-08-07 18:24:18 +0100

    Changed huggingface link for BART

--- backend/models/summarizer_models.py
+++ backend/models/summarizer_models.py
@@ -3,7 +3,7 @@ import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-tokenizer_bart = AutoTokenizer.from_pretrained("pramjati02/Bart-Base-Trained")
+tokenizer_bart = AutoTokenizer.from_pretrained("pramjati02/bart-base-trained")
 model_bart = AutoModelForSeq2SeqLM.from_pretrained("pramjati02/Bart-Base-Trained").to(device)
 
 tokenizer_t5 = AutoTokenizer.from_pretrained("pramjati02/t5-base-trained")
